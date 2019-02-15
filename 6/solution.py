56be0cab95be647c21e3442d11c5985014b99701
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-02-15 17:42:19 -0600

    Day 6 Part 1

---
+++ 6/solution.py
@@ -0,0 +1,37 @@
+from operator import itemgetter
+
+def redistribute(intList):
+    new_list = intList[:]
+    indices_and_values = enumerate(intList)
+    max_index, max_value = max(indices_and_values,key=itemgetter(1))
+    d,r = divmod(max_value,len(intList)-1)
+    if d == 0:
+        for i in range(max_index+1,max_index+max_value+1):
+            new_list[i%len(new_list)] += 1
+        new_list[max_index] = 0
+    else:
+        for i in range(len(new_list)):
+            if i != max_index:
+                new_list[i] += d
+            else:
+                new_list[i] = r
+    return new_list
+
+def part_one(intList):
+    new_list = intList[:]
+    configurations = set() # can't hash lists so we will cast to string
+    loops = 0
+    while True:
+        loops += 1
+        new_list = redistribute(new_list)
+        if str(new_list) in configurations:
+            return loops
+        else:
+            configurations.add(str(new_list))
+            
+
+if __name__ == "__main__":
+    with open("input.txt") as file:
+        text = file.read()
+    array = [int(x) for x in text.split()]
+    print(part_one(array))
---
+++ 6/test_day6.py
@@ -0,0 +1,16 @@
+import solution as s
+
+def test_redistribute():
+    examples = [
+        [0,2,7,0],
+        [2,4,1,2],
+        [3,1,2,3],
+        [0,2,3,4],
+        [1,3,4,1],
+        [2,4,1,2]
+    ]
+    for i in range(0,len(examples)-1):
+        assert s.redistribute(examples[i]) == examples[i+1]
+
+def test_part_one():
+    assert s.part_one([0,2,7,0]) == 5
