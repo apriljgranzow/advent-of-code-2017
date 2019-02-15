373e91857cd346281293d9f5b3cad421be918561
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-02-14 23:19:35 -0600

    Finished part 2

--- 4/solution.py
+++ 4/solution.py
@@ -14,7 +14,24 @@ def part_one(stringList):
             total += 1
     return total
 
+def no_anagram_words(string):
+    anagrams = set()
+    for word in string.split():
+        if ''.join(sorted(word)) in anagrams:
+            return False
+        else:
+            anagrams.add(''.join(sorted(word)))
+    return True
+
+def part_two(stringList):
+    total = 0
+    for string in stringList:
+        if no_anagram_words(string):
+            total += 1
+    return total
+
 if __name__ == "__main__":
     with open('input.txt') as file:
         lines = file.readlines()
     print(part_one(lines))
+    print(part_two(lines))
--- 4/test_day4.py
+++ 4/test_day4.py
@@ -9,3 +9,13 @@ def test_no_dupe_words():
     for elem in examples:
         assert s.no_dupe_words(elem) == examples[elem]
 
+def test_no_anagrams():
+    examples = {
+        'abcde fghij' : True,
+        'abcde xyz ecdab' : False,
+        'a ab abc abd abf abj' : True,
+        'iiii oiii ooii oooi oooo' : True,
+        'oiii ioii iioi iiio' : False
+    }
+    for elem in examples:
+        assert s.no_anagram_words(elem) == examples[elem]
