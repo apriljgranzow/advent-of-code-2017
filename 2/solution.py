9b766593940d57435ebb2c1501799479125d8770
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-02-14 22:16:40 -0600

    Day 2 Part 2

--- 2/solution.py
+++ 2/solution.py
@@ -7,6 +7,20 @@ def part_one(inputs):
         total += range_checksum(line)
     return total
 
+def find_even_pair_quotient(lst):
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if i != j and (lst[i] % lst[j] == 0):
+                return lst[i] / lst[j]
+            elif i != j and (lst[j] % lst[i] == 0):
+                return lst[j] / lst[i]
+
+def part_two(inputs):
+    total = 0
+    for line in inputs:
+        total += find_even_pair_quotient(line)
+    return total
+
 if __name__ == "__main__":
     with open('input.txt') as file:
         lines = file.readlines()
@@ -14,3 +28,4 @@ if __name__ == "__main__":
     for line in lines:
         intLists.append([int(x) for x in line.split()])
     print(part_one(intLists))
+    print(part_two(intLists))
--- 2/test_day2.py
+++ 2/test_day2.py
@@ -11,3 +11,15 @@ def test_range_checksum():
 
 def test_part_one():
     assert s.part_one([[5,1,9,5],[7,5,3],[2,4,6,8]]) == 18
+
+def test_find_even_pair_quotient():
+    examples = {
+        (5,9,2,8) : 4,
+        (9,4,7,3) : 3,
+        (3,8,6,5) : 2
+    }
+    for elem in examples:
+        assert s.find_even_pair_quotient(elem) == examples[elem]
+
+def test_part_two():
+    assert s.part_two([(5,9,2,8),(9,4,7,3),(3,8,6,5)]) == 9
