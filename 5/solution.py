3a6dcdba574ceb9bd37b31df454a1bfd3e26de7c
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-02-15 00:22:06 -0600

    Fixed issue with mutating input array

--- 5/solution.py
+++ 5/solution.py
@@ -1,11 +1,26 @@
 from collections import defaultdict
 
 def part_one(intList):
+    array = intList[:]
     steps = 0
     i = 0
-    while i < len(intList):
-        jump_value = (intList[i])
-        intList[i] += 1
+    while i < len(array):
+        jump_value = (array[i])
+        array[i] += 1
+        i += jump_value
+        steps += 1
+    return steps
+    
+def part_two(intList):
+    array = intList[:]
+    steps = 0
+    i = 0
+    while i < len(array):
+        jump_value = (array[i])
+        if array[i] >= 3:
+            array[i] -= 1
+        else:
+            array[i] += 1
         i += jump_value
         steps += 1
     return steps
@@ -15,3 +30,4 @@ if __name__ == "__main__":
         lines = file.readlines()
     inputs = [int(x) for x in lines]
     print(part_one(inputs))
+    print(part_two(inputs))
--- 5/test_day5.py
+++ 5/test_day5.py
@@ -2,3 +2,6 @@ import solution as s
 
 def test_part_one():
     assert s.part_one([0,3,0,1,-3]) == 5
+
+def test_part_two():
+    assert s.part_two([0,3,0,1,-3]) == 10
