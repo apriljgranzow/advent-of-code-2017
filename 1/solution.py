1f44fe9a75a341e4fd28bb1237f43322c79888ee
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-02-14 21:22:33 -0600

    Part 2 with unit tests

--- 1/solution.py
+++ 1/solution.py
@@ -6,7 +6,20 @@ def part_one(intStr):
             total += int(intStr[i])
     return total
 
+def groupby_indices_halfway_round(iterable):
+    median = len(iterable)//2
+    return list(zip(iterable[:median],iterable[median:]))
+
+def part_two(intStr):
+    tuple_list = groupby_indices_halfway_round(intStr)
+    total = 0
+    for pair in tuple_list:
+        if pair[0] == pair[1]:
+            total += int(pair[0])*2
+    return total
+
 if __name__ == "__main__":
     with open('input.txt') as file:
         inputStr = file.read()
     print(part_one(inputStr))
+    print(part_two(inputStr))
--- 1/test_day1.py
+++ 1/test_day1.py
@@ -9,3 +9,25 @@ def test_part_one():
     }
     for elem in examples:
         assert s.part_one(elem) == examples[elem]
+
+def test_groupby_indices_halfway_round():
+    examples = {
+        '1212' : [('1','1'),('2','2')],
+        '1221' : [('1','2'),('2','1')],
+        '123425' : [('1','4'), ('2','2'),('3','5')],
+        '123123' : [('1','1'), ('2','2'),('3','3')],
+        '12131415' : [('1','1'), ('2','4'),('1','1'),('3','5')]
+    }
+    for elem in examples:
+        assert s.groupby_indices_halfway_round(elem) == examples[elem]
+
+def test_part_two():
+    examples = {
+        '1212' : 6,
+        '1221' : 0,
+        '123425' : 4,
+        '123123' : 12,
+        '12131415' : 4
+    }
+    for elem in examples:
+        assert s.part_two(elem) == examples[elem]
