3a30ec8a4b801cdc95ebb9072c7dcfb30aa2635a
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-02-26 02:09:31 -0600

    Day 9 Part 2 with unit tests

--- 9/solution.py
+++ 9/solution.py
@@ -23,7 +23,26 @@ def part_one(charstream):
                 depth -= 1
     return score
 
+def part_two(charstream):
+    garbage = False
+    ignore = False
+    removed = 0
+    for char in charstream:
+        if garbage == False and char == '<':
+            garbage = True
+        elif garbage == True:
+            if not ignore and char == '!':
+                ignore = True
+            elif ignore == True:
+                ignore = False
+            elif not ignore and char == '>':
+                garbage = False
+            else:
+                removed += 1
+    return removed
+
 if __name__ == "__main__":
     with open("input.txt") as file:
         text = file.read()
     print(part_one(text))
+    print(part_two(text))
--- 9/test_day9.py
+++ 9/test_day9.py
@@ -13,3 +13,16 @@ def test_part_one():
     }
     for elem in examples:
         assert s.part_one(elem) == examples[elem]
+
+def test_part_two():
+    examples = {
+	'<>' :  0,
+	'<random characters>' :  17,
+	'<<<<>' :  3,
+	'<{!>}>' :  2,
+	'<!!>' :  0,
+	'<!!!>>' :  0,
+	'<{o"i!a,<{i<a>' :  10
+    }
+    for elem in examples:
+        assert s.part_two(elem) == examples[elem]
