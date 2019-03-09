5717372e0e37c799baa57659aba63ec22650183d
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-03-08 22:10:14 -0600

    Day 12 Part 2

--- 12/solution.py
+++ 12/solution.py
@@ -29,7 +29,23 @@ def part_one(text):
     g = create_graph(get_edge_tuples(regex_input(text)))
     return len(list(nx.dfs_preorder_nodes(g, 0)))
 
+def group_count(graph):
+    '''Given an undirected graph, find how many distinct groups there
+    are with no connection to each other'''
+    visited = set()
+    groups = 0
+    for node in graph.nodes:
+        if node not in visited:
+            visited.update(list(nx.dfs_preorder_nodes(graph, node)))
+            groups += 1
+    return groups
+
+def part_two(text):
+    g = create_graph(get_edge_tuples(regex_input(text)))
+    return group_count(g)
+
 if __name__ == "__main__":
     with open("input.txt") as file:
         text = file.read()
     print(part_one(text))
+    print(part_two(text))
